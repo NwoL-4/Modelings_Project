52aef172d89178a565e6db0b2dd0289ed4ed93e5
NwoL-4 <ImPerfect-18@yandex.ru>
2025-03-29 20:28:37 +0300

    Добавил функции для общего layout и генерации графика и фреймов для nbody

--- utils/plot_generators.py
+++ utils/plot_generators.py
@@ -1,31 +1,85 @@
 import time
 
+import numpy as np
+import plotly.graph_objects as go
 
-def generate_frame(subplots_data):
-    """Генерирует фрейм для обновления субплотин
+from constants import ui_constants
 
-    Args:
-        subplots_data: dict[tuple, dict]
-            (row, col) -> {x: list, y: list, traces: list} 
-    """
-    frame = {
-        'type': 'frame',
-        'timestamp': time.time(),
-        'subplots': []
-    }
 
-    for (row, col), data in subplots_data.items():
-        frame['subplots'].append({
-            'grid': {'row': row, 'col': col},
-            'data': [
-                {
-                    'x': trace_data['x'],
-                    'y': trace_data['y'],
-                    'mode': trace_data.get('mode', 'lines'),
-                    'name': trace_data.get('name')
-                } for trace_data in data['traces']
-            ],
-            'layout': data.get('layout', {})
-        })
+def create_general_layout():
+    return go.Layout(
+        font=dict(
+            family=ui_constants.MAIN_FONT,
+            size=ui_constants.FONT_SIZE
+        ),
+        updatemenus=[
+            dict(
+                type="buttons",
+                direction="left",
+                xanchor="center",
+                yanchor="top",
+                x=1,
+                y=0.5,
+                showactive=False,
+                buttons=[
+                    dict(
+                        label="►",
+                        method="animate",
+                        args=[None, {
+                            'frame': {'duration': 5, 'redraw': True},
+                            'fromcurrent': True,
+                            "mode": "immediate",
+                            'transition': {'duration': 5}
+                        }]
+                    ),
+                    dict(
+                        label="❚❚",
+                        method="animate",
+                        args=[[None], {
+                            'frame': {'duration': 15, 'redraw': False},
+                            'mode': 'immediate',
+                            'transition': {'duration': 0}
+                        }]
+                    )
+                ]
+            )
+        ]
+    )
 
-    return frame
+
+def generate_markers_nbody(data: np.ndarray, colors: list):
+    return [go.Scatter3d(
+        x=[data[0][body]],
+        y=[data[1][body]],
+        z=[data[2][body]],
+        mode='markers',
+        marker=dict(
+            size=20,  # Размер маркеров
+            symbol='circle',  # Тип маркера (круг)
+            color=colors[body],
+        ),
+        name=f'Тело {body + 1}',  # Подписи при наведении
+        hoverinfo='name+x+y+z',  # Показываем координаты и текст при наведении
+        showlegend=True
+    )
+        for body in range(data.shape[1])
+    ]
+
+
+def generate_lines_nbody(data: np.ndarray, colors: list):
+    return [go.Scatter3d(
+        x=data[:, 0, body],
+        y=data[:, 1, body],
+        z=data[:, 2, body],
+        mode='lines',
+        line=dict(
+            width=10,
+            color=colors[body],
+        ),
+        text=f'Тело {body + 1}',  # Подписи при наведении
+        hoverinfo='skip',  # Показываем координаты и текст при наведении
+        showlegend=False
+    )
+    for body in range(data.shape[2])
+]
+
